2054842f690923445424db484cac7efde4555838
Darya Volkova <125033233+DaryaVolkova@users.noreply.github.com>
2023-02-19 16:55:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,14 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int n;
-    int max = a > b ? a : b;
-    for (int i = 1; i <= max; i++){
-        if ((a % i == 0) && (b % i == 0)) n = i;
-    }
-    return n;
+    if (a < 0) a = 0 - a;
+	if (b < 0) b = 0 - b;
+	if ((a != 0) && (b != 0)) {
+		while (a != b) {
+			if (a > b) a = a - b;
+			else b = b - a;
+		}
+		return a;
+	}
+	else return (a + b);
 }
