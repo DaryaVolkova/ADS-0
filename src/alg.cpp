99f45934c0acec674a85650fde0d3a626e0f09d0
Darya Volkova <125033233+DaryaVolkova@users.noreply.github.com>
2023-02-19 17:04:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,13 @@
 
 int gcd(int a, int b) {
     if (a < 0) a = 0 - a;
-	if (b < 0) b = 0 - b;
-	if ((a != 0) && (b != 0)) {
-		while (a != b) {
-			if (a > b) a = a - b;
-			else b = b - a;
-		}
-		return a;
-	}
-	else return (a + b);
+    if (b < 0) b = 0 - b;
+    if ((a != 0) && (b != 0)) {
+        while (a != b) {
+            if (a > b) a = a - b;
+            else b = b - a;
+        }
+    return a;
+    }
+    else return (a + b);
 }
